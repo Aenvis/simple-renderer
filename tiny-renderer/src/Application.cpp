7a7e610083be6d00bf23639e4d0ae0e3774ef024
Wojtek Czerwinski <czerwinskiwtj@gmail.com>
2022-09-26 23:36:44 +0200

    added DrawTriangle

--- tiny-renderer/src/Application.cpp
+++ tiny-renderer/src/Application.cpp
@@ -1,4 +1,4 @@
-#include "tgaimage.h"
+﻿#include "tgaimage.h"
 #include "Model.h"
 #include "Geometry.h"
 #define IMAGE_WIDTH 800			
@@ -8,15 +8,7 @@ const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
 const TGAColor green = TGAColor(0, 255, 0, 255);
 
-struct Vector2
-{
-	int x = 0, y = 0;
-
-	Vector2(int x,int y)
-		: x(x), y(y) {}
-};
-
-void DrawLine(Vector2 p0, Vector2 p1, TGAImage& image, const TGAColor& color) {
+void DrawLine(Vector2i p0, Vector2i p1, TGAImage& image, const TGAColor& color) {
 	bool steep = false;
 	if (std::abs(p0.x - p1.x) < std::abs(p0.y - p1.y))
 	{
@@ -63,29 +55,40 @@ void DrawLine(Vector2 p0, Vector2 p1, TGAImage& image, const TGAColor& color) {
 		}
 	}
 }
-int main() {
-	Model* model = new Model("models/model.obj");
 
+void DrawTriangle(Vector2i p0, Vector2i p1, Vector2i p2, TGAImage& image)
+{
+	//sort vertices in ascending y order
+	if (p0.y > p1.y) std::swap(p0, p1);
+	if (p0.y > p2.y) std::swap(p0, p2);
+	if (p1.y > p2.y) std::swap(p1, p2);
+	 
+	int totalHeight = p2.y - p0.y;
+
+	for(int y = p0.y; y <= p1.y; y++)
+	{
+		int segmentHeight = p1.y - p0.y + 1;
+		float alpha = (float)(y - p0.y) / totalHeight;
+		float beta = (float)(y - p0.y) / segmentHeight;
+		Vector2i _p0 = p0 + (p2 - p0) * alpha;
+		Vector2i _p1 = p0 + (p1 - p0) * beta;
+		image.set_pixel_color(_p0.x, y, red);
+		image.set_pixel_color(_p1.x, y, green);
+	}
+}
+
+int main() 
+{
+	Vector2i t0[3] = { Vector2i(50, 50), Vector2i(200, 100), Vector2i(100, 600)};
+	Vector2i t1[3] = { Vector2i(700, 100), Vector2i(500, 623), Vector2i(699, 500)};
+	Vector2i t2[3] = { Vector2i(400, 400), Vector2i(500, 320), Vector2i(399, 600)};
 	TGAImage image(IMAGE_WIDTH, IMAGE_HEIGHT, TGAImage::RGB);
 
-	for (size_t i = 0; i < model->numberOfFaces(); i++)
-	{
-		std::vector<int> face = model->GetFace(i);
-		
-		for (size_t j = 0; j < 3; j++)
-		{
-			Vector3f v0 = model->GetFaceVertices(face[j]);
-			Vector3f v1 = model->GetFaceVertices(face[(j + 1) % 3]);
-			int x0 = (v0.x + 1.0f) * IMAGE_WIDTH / 2;
-			int y0 = (v0.y + 1.0f) * IMAGE_HEIGHT / 2;
-			int x1 = (v1.x + 1.0f) * IMAGE_WIDTH / 2;
-			int y1 = (v1.y + 1.0f) * IMAGE_HEIGHT / 2;
-			DrawLine({ x0, y0 }, { x1, y1 }, image, white);
-		}
-	}
+	DrawTriangle(t0[0], t0[1], t0[2], image);
+	DrawTriangle(t2[0], t2[1], t2[2], image);
+	DrawTriangle(t1[0], t1[1], t1[2], image);
+
 	image.flip_vertically(); // to have the origin at the left bottom corner of the image
 	image.write_tga_file("output.tga");
-
-	delete model;
 }
 
